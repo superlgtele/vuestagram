133151e9f561eff6ad848eed3cfe6250eeefc6ed
superlgtele <superlgtele@naver.com>
2022-01-02 00:28:16 +0900

    Study with vue_site
    2022/01/02

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <UsePost/>
+      <UsePost/>
+      <UsePost/>
+  </div>
+</template>
+
+<script>
+import Post from './Post'
+
+export default {
+    components : {
+      UsePost : Post
+    }
+}
+</script>
+
+<style>
+
+</style>
